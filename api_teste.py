199fb4dd31236368123f07011209405d3ff7ce44
Pamela Lima <pamela.jesus.lima@gmail.com>
2023-05-18 19:32:02 -0300

    testando soluções para o problema
    não achei

--- api_teste.py
+++ api_teste.py
@@ -1,29 +1,33 @@
-from flask import Flask, request, json
+from flask import Flask
+import requests
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/aula/', methods=["GET"])
+@app.route('/tipo', methods=["GET"])
 def home():
     api_url = "https://jsonplaceholder.typicode.com/todos/1"
-    response = request.get(api_url)
+    response = requests.get(api_url)
     return response.json()
 
 
-@app.route('/aula', methods=["POST"])
+@app.route('/tipo/v1', methods=["POST"])
 def cadastrar():
     api_url = "https://jsonplaceholder.typicode.com/todos"
     enviar = {"userId": 1, "title": "Buy milk", "completed": False}
-    response = request.post(api_url, json=enviar)
+    response = requests.post(api_url, json=enviar)
     return response.json()
 
 
-# @app.route('/v1/tipo', methods=["DELETE"])
-# def home():
-#     api_url = "https://jsonplaceholder.typicode.com/todos/1"
-#     response = request.delete(api_url)
-#     return response.json()
+@app.route('/v1/tipo', methods=["DELETE"])
+def home():
+    api_url = "https://jsonplaceholder.typicode.com/todos/1"
+    response = requests.delete(api_url)
+    return response.json()
 
+#pip install requests - comando no terminal
+#https://www.treinaweb.com.br/blog/consumindo-apis-com-python-parte-1
 
 if __name__ == '__main__':
     app.run(debug=True)
